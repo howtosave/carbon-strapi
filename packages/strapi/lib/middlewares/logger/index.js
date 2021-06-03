33c32f0ec7b67fd53ec7589d189fdca8d9be8768
peterk <tiglek+u16office@gmail.com>
2021-06-04 00:55:21 +0900

    - update logger middleware

--- packages/strapi/lib/middlewares/logger/index.js
+++ packages/strapi/lib/middlewares/logger/index.js
@@ -55,7 +55,12 @@ module.exports = strapi => {
           const start = Date.now();
           await next();
           const delta = Math.ceil(Date.now() - start);
-          strapi.log.debug(`${ctx.method} ${ctx.url} (${delta} ms) ${codeToColor(ctx.status)}`);
+          const userId = ctx.state && ctx.state.user && ctx.state.user.id;
+          strapi.log.debug(`${codeToColor(ctx.status)} ${ctx.method} ${ctx.url} (${delta} ms) > ${userId ? chalk.gray(userId) : ''}`);
+          if (strapi.log.levelVal <= 10) { // trace
+            if (ctx.request.length > 0) strapi.log.trace('req body:', ctx.request.body);
+            if (ctx.response.body) strapi.log.trace('res body:', ctx.response.body);
+          }
         });
       }
     },
