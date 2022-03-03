9889cf66eca94b490774b062dd1b5ea4eba783dc
peterk <tiglek+u16office@gmail.com>
2022-03-03 18:38:27 +0900

    update admin application-info page

--- packages/strapi-admin/admin/src/containers/ApplicationInfosPage/index.js
+++ packages/strapi-admin/admin/src/containers/ApplicationInfosPage/index.js
@@ -8,28 +8,14 @@ import { BaselineAlignment } from 'strapi-helper-plugin';
 import Bloc from '../../components/Bloc';
 import PageTitle from '../../components/SettingsPageTitle';
 import makeSelectApp from '../App/selectors';
-import makeSelectAdmin from '../Admin/selectors';
 import { Detail, InfoText } from './components';
 
 const makeSelectAppInfos = () => createSelector(makeSelectApp(), appState => appState.appInfos);
-const makeSelectLatestRelease = () =>
-  createSelector(makeSelectAdmin(), adminState => ({
-    latestStrapiReleaseTag: adminState.latestStrapiReleaseTag,
-    shouldUpdateStrapi: adminState.shouldUpdateStrapi,
-  }));
 
 const ApplicationInfosPage = () => {
   const { formatMessage } = useIntl();
   const selectAppInfos = useMemo(makeSelectAppInfos, []);
-  const selectLatestRealase = useMemo(makeSelectLatestRelease, []);
   const appInfos = useSelector(state => selectAppInfos(state));
-  const { shouldUpdateStrapi, latestStrapiReleaseTag } = useSelector(state =>
-    selectLatestRealase(state)
-  );
-
-  const currentPlan = appInfos.communityEdition
-    ? 'app.components.UpgradePlanModal.text-ce'
-    : 'app.components.UpgradePlanModal.text-ee';
 
   const headerProps = {
     title: { label: formatMessage({ id: 'Settings.application.title' }) },
@@ -37,20 +23,7 @@ const ApplicationInfosPage = () => {
       id: 'Settings.application.description',
     }),
   };
-  const pricingLabel = formatMessage({ id: 'Settings.application.link-pricing' });
-  const upgradeLabel = formatMessage({ id: 'Settings.application.link-upgrade' });
-  const strapiVersion = formatMessage({ id: 'Settings.application.strapi-version' });
   const nodeVersion = formatMessage({ id: 'Settings.application.node-version' });
-  const editionTitle = formatMessage({ id: 'Settings.application.edition-title' });
-
-  /* eslint-disable indent */
-  const upgradeLink = shouldUpdateStrapi
-    ? {
-        label: upgradeLabel,
-        href: `https://github.com/strapi/strapi/releases/tag/${latestStrapiReleaseTag}`,
-      }
-    : null;
-  /* eslint-enable indent */
 
   return (
     <div>
@@ -60,18 +33,24 @@ const ApplicationInfosPage = () => {
       <Bloc>
         <Padded left right top size="smd">
           <Padded left right top size="xs">
-            <Flex justifyContent="space-between">
-              <Detail
-                link={upgradeLink}
-                title={strapiVersion}
-                content={`v${appInfos.strapiVersion}`}
-              />
-              <Detail
-                link={{ label: pricingLabel, href: 'https://strapi.io/pricing' }}
-                title={editionTitle}
-                content={formatMessage({ id: currentPlan })}
-              />
-            </Flex>
+            <Padded top size="lg">
+              <Text fontSize="xs" color="grey" fontWeight="bold">
+                App Package Name
+              </Text>
+              <InfoText content={appInfos.appPackageName} />
+            </Padded>
+            <Padded top size="lg">
+              <Text fontSize="xs" color="grey" fontWeight="bold">
+                App Package Version
+              </Text>
+              <InfoText content={appInfos.appPackageVersion} />
+            </Padded>
+            <Padded top size="lg">
+              <Text fontSize="xs" color="grey" fontWeight="bold">
+                Environment
+              </Text>
+              <InfoText content={appInfos.currentEnvironment} />
+            </Padded>
             <Padded top size="lg">
               <Text fontSize="xs" color="grey" fontWeight="bold">
                 {nodeVersion}
